72d67cf4c6d46b2642fa802cee2d0570f1d4223e
Sam Smith <sam@example.com>
2016-05-31 17:01:38 -0600

    and done

--- matching.js
+++ matching.js
@@ -1,6 +1,21 @@
 var matchingElement = document.getElementById('matchingGame');
 
-var matchingCards = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
+var matchingCards = [];
+var flips = 0
+var flippedCard = ''
+var flippedCard1 = ''
+var flippedcard2 = ''
+var cardCount = 0
+
+var reset = function reset() {
+matchingCards = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
+// matchingCards = ['red', 'green'];
+flips = 0
+flippedCard = ''
+flippedCard1 = ''
+flippedcard2 = ''
+cardCount = 0
+}
 
 var buildDeck = function buildDeck() {
     matchingCards = matchingCards.concat(matchingCards)
@@ -20,17 +35,58 @@ var buildBoard = function buildBoard() {
     matchingElement.innerHTML = ''
     var buildDeck = ''
     for (var i = 0; i < matchingCards.length; i++) {
-        console.log(matchingCards[i]);
-      var deck = '<div class = "cards" onclick = "flipCard(this, \''+matchingCards[i]+'\')"></div>';
+      var deck = '<div id = "card'+i+'" class = "cards" style = "background:white" onclick = "flipCard(this, \''+matchingCards[i]+'\')"></div>';
       buildDeck += deck;
     }
     matchingElement.innerHTML = buildDeck;
 };
 
 var flipCard = function flipcard(card, color) {
-    card.style.background = color;
+    if (flips == 0 && card.style.background == 'white') {
+        flips = 1;
+        flippedCard = color;
+        flippedCard1 = card;
+        card.style.transform = 'rotateY(180deg)';
+        card.style.background = color;
+    } else if (flips == 1 && flippedCard !== color && card.style.background == 'white') {
+        var flipBack = function flipBack() {
+            flips = 0;
+            flippedCard1.style.background = 'white';
+            flippedCard2.style.background = 'white';
+            flippedCard1.style.transform = '';
+            flippedCard2.style.transform = '';
+        }
+        flips = 2;
+        flippedCard2 = card;
+        card.style.transform = 'rotateY(180deg)';
+        card.style.background = color;
+        setTimeout(flipBack, 700);
+    } else if (flips == 1 && flippedCard == color && card.style.background == 'white') {
+        card.style.transform = 'rotateY(180deg)';
+        card.style.background = color;
+        flips = 0;
+        cardCount += 2;
+        if (cardCount == matchingCards.length) {
+            var victory = function victory() {
+                sweetAlert ("Congrats!", "You did it!", "success");
+                newgame();
+            }
+            $('.cards').velocity("callout.flash", { stagger: 100, duration: 500 })
+            $('.cards').velocity("callout.bounce", { stagger: 100, duration: 500 })
+            setTimeout(victory, 3500);
+            
+        }
+    }
+        
+        
 }
-
+var newgame = function newgame() {
+reset();
 buildDeck();
 shuffleDeck();
 buildBoard();
+}
+
+newgame();
+
+
